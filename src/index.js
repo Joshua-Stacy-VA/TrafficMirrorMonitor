1f230da6a7f3ff0e0dbfba7ae86465992f34415e
Mike Furoyama <mfuroyama@hawaiirg.com>
2019-10-11 10:49:09 -1000

    Add comments that will hopefully help me remember

--- src/index.js
+++ src/index.js
@@ -7,13 +7,15 @@ const { Process } = require('./process');
 const { Store } = require('./store');
 const { Sessions } = require('./sessions');
 
-
+// Set up the system services via dependency injection handler
 const system = new Bottle();
 system.service('Configuration', Configuration);
 system.service('Sessions', Sessions);
 system.factory('Logger', Logger.createLogger);
 system.factory('Process', Process.createProcess);
 system.factory('Store', Store.createStore);
+
+// Instantiate the following services immediately here
 system.digest(['Process']);
 
 
