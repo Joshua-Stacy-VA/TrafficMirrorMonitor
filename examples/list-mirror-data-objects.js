5f0b3cd1fe8001dc45fdc57f9082d6b248489703
Mike Furoyama <mfuroyama@hawaiirg.com>
2020-01-27 09:51:40 -1000

    Add utility to list S3 objects for a given day

---
+++ examples/list-mirror-data-objects.js
@@ -0,0 +1,101 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const fs = require('fs-extra');
+const { DateTime } = require('luxon');
+
+// ============================================= Configuration properties ==============================================
+const args = require('minimist')(process.argv.slice(2), {
+    alias: {
+        b: 'bucket', d: 'startDate', z: 'timezone', r: 'region',
+    },
+    default: {
+        startDate: DateTime.local().toFormat('M/d/yyyy'),
+        timezone: 'America/Chicago',
+        region: 'us-gov-east-1',
+    },
+});
+
+const {
+    bucket, startDate, timezone, region,
+} = args;
+
+// ===================================================== Utilities =====================================================
+const makeNiceTimestamp = timestamp => DateTime.fromMillis(timestamp).toFormat('M/d/yyyy');
+
+const getTimestampRange = () => {
+    const dateTime = DateTime.fromFormat(startDate, 'M/d/yyyy').setZone(timezone);
+
+    if (!dateTime.isValid) {
+        throw new Error(`Invalid start date "startDate" (-d) argument ${startDate}, must be M/D/YYYY`);
+    }
+
+    return [
+        dateTime.startOf('day').toMillis(),
+        dateTime.endOf('day').toMillis(),
+    ];
+};
+
+// ================================================== Initialization ===================================================
+if (!bucket) {
+    throw new Error('AWS S3 bucket name "bucket" (-b) argument required');
+}
+
+const s3 = new AWS.S3({ region });
+const [startTimestamp, endTimestamp] = getTimestampRange();
+
+// =================================================== Read from S3 ====================================================
+const listObjects = async () => {
+    const readS3 = async (objects = [], nextToken = null) => {
+        const params = {
+            Bucket: bucket,
+            Prefix: startTimestamp.toString(),
+        };
+        if (nextToken) {
+            Object.assign(params, { ContinuationToken: nextToken });
+        }
+
+        try {
+            const listResults = await s3.listObjectsV2(params).promise();
+            const { IsTruncated: isTruncated, Contents: contents } = listResults;
+
+            const filteredContents = contents.filter((object) => {
+                const { Key: key } = object;
+                const [token] = key.split('_')[0];
+                const timestamp = +token;
+
+                return (!Number.isNaN(timestamp) && timestamp >= startTimestamp && timestamp <= endTimestamp);
+            }).map(object => object.Key);
+
+            if (filteredContents.length === 0) {
+                return objects;
+            }
+
+            objects.push(...filteredContents);
+
+            if (isTruncated) {
+                const { NextContinuationToken: token } = listResults;
+                await readS3(objects, token);
+            }
+        } catch (err) {
+            console.log(`Error reading objects from bucket ${bucket}`);
+            console.log(err.toString());
+            console.log(err.stack);
+            throw err;
+        }
+
+        return objects;
+    };
+
+    const results = await readS3();
+    return results;
+};
+
+(async () => {
+    console.log(`Reading S3 bucket ${bucket} for objects between ${makeNiceTimestamp(startTimestamp)} to ${makeNiceTimestamp(endTimestamp)}`);
+    const objects = await listObjects();
+
+    const outputFileName = `s3-objects_${startTimestamp}-${endTimestamp}.txt`;
+    console.log(`Saving ${objects.length} objects to ${outputFileName}`);
+    await fs.outputFile(outputFileName, objects.join('\n'));
+})();
